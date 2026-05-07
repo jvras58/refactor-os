b8ea17f7cc3f3dbf2e6d0a52f213bd6973cb285a
jvras58 <jvras58@gmail.com>
2026-05-07 10:39:05 -0300

    chore: remove unnecessary blank line in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 PROJECT_ROOT = Path(__file__).resolve().parent.parent.parent
 
 
